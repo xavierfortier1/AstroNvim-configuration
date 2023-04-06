63716efccf12dbd3f6153009b1cb23f9eba026ad
Xav-1 <xabellechasse20@gmail.com>
2023-04-06 17:27:44 -0400

    Changement du <C-s>

--- mappings.lua
+++ mappings.lua
@@ -21,7 +21,7 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+    ["<C-s>"] = { "<cmd>w!<cr>", desc = "Save File" }, -- change description but the same command
   },
   t = {
     -- setting a mapping to false will disable it
