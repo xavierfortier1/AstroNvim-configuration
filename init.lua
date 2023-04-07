e04645df1f0ca88a162037ddf56a711dfa9a3ddc
Xav-1 <xabellechasse20@gmail.com>
2023-04-07 13:37:24 -0400

    Various fixes

--- init.lua
+++ init.lua
@@ -86,7 +86,6 @@ return {
       }
     end,
   },
-
   icons = {
     VimIcon = "",
     ScrollText = "",
@@ -124,7 +123,6 @@ return {
       },
     },
   },
-
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,26 +1,11 @@
--- TODO finish
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-
-  -- Todo highlight
   {
     "folke/todo-comments.nvim",
     dependencies = "nvim-lua/plenary.nvim",
     opts = {},
     event = "User AstroFile",
     cmd = { "TodoQuickFix" },
-    keys = {
-      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
-    },
+    keys = {},
   },
 
   -- NvChad StatusLine
