d10b70727c955ce2009c2f29e4ed392edcd04af4
Xav-1 <xabellechasse20@gmail.com>
2023-04-07 13:38:41 -0400

    Brackets colorizer et "<leader>c"

--- mappings.lua
+++ mappings.lua
@@ -20,8 +20,15 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    ["<C-s>"] = { "<cmd>w!<cr>", desc = "Save File" }, -- change description but the same command
+    -- Open Alpha automatically when no more buffers
+    ["<leader>c"] = {
+      function()
+        local bufs = vim.fn.getbufinfo { buflisted = true }
+        require("astronvim.utils.buffer").close(0)
+        if require("astronvim.utils").is_available "alpha-nvim" and not bufs[2] then require("alpha").start(true) end
+      end,
+      desc = "Close buffer",
+    },
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/community.lua
+++ plugins/community.lua
@@ -4,9 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
-  -- { import = "astrocommunity.colorscheme.tokyonight" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
