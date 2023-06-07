e749436c6f6fb1cce243b20e563bf41c41c15dee
Xavier Fortier <xabellechasse20@gmail.com>
2023-06-06 21:02:04 -0400

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,9 +5,21 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.colorscheme.nightfox" },
+
+  -- Colorscheme
+  { import = "astrocommunity.colorscheme.iceberg-vim",         enable = false },
+  { import = "astrocommunity.colorscheme.nightfox-nvim",       enable = false },
+  { import = "astrocommunity.colorscheme.mellow-nvim",         enable = true },
+
+  -- Editing support
   { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+
+  -- Pack
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
+
+  -- Utility
+  { import = "astrocommunity.utility.noice-nvim" },
 }
