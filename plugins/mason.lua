dc3c0c3e66db8221d97c8c3f895b4f16a6aed7ab
Xavier Fortier <xabellechasse20@gmail.com>
2023-06-08 20:34:40 -0400

    Removed all ensured_install

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "pyright", "rust_analyzer" },
+      ensure_installed = {},
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,14 +13,14 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      ensure_installed = { "prettier", "stylua", "rustfmt" },
+      ensure_installed = {},
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      ensure_installed = { "python", "lua", "rust" },
+      ensure_installed = {},
     },
   },
 }
