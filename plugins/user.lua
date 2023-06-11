cfd0467abcb8d6702e8f38a78b31e3d079ed7b7b
Xavier Fortier <xabellechasse20@gmail.com>
2023-06-11 00:21:41 -0400

    Fixed spaces issues

--- plugins/user.lua
+++ plugins/user.lua
@@ -76,7 +76,7 @@ return {
     "loctvl842/monokai-pro.nvim",
     opts = {
       terminal_colors = true,
-      devicons = true,     -- highlight the icons of `nvim-web-devicons`
+      devicons = true, -- highlight the icons of `nvim-web-devicons`
       filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
       plugins = {
         indent_blankline = {
