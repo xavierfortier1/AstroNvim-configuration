6772fe63287bfd395ed52e297deb065151b44d57
Xavier Fortier <xabellechasse20@gmail.com>
2023-06-20 21:25:14 -0400

    Added peek.nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,25 +4,29 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- Colorscheme
+  -- colorscheme
 
-  -- Completion
+  -- completion
 
-  --Debugging
+  --debugging
 
-  -- Editing support
+  -- editing support
   { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
 
-  -- Pack
+  -- mardown-and-latex
+  { import = "astrocommunity.markdown-and-latex.peek-nvim" },
+
+  -- pack
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.markdown" },
 
-  -- Scrolling
+  -- scrolling
   { import = "astrocommunity.scrolling.neoscroll-nvim" },
 
-  -- Utility
+  -- utility
   { import = "astrocommunity.utility.noice-nvim" },
 }
