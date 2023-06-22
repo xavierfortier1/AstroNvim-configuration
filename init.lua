0a78ad8ead06634dfd806cfb139387d1cfc0995f
Xavier Fortier <xabellechasse20@gmail.com>
2023-06-21 20:34:34 -0400

    Changed formatting
    format_on_save for all files

--- init.lua
+++ init.lua
@@ -28,11 +28,8 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          "python",
-          "lua",
-          "rust",
+        enabled = true,      -- enable or disable format on save globally
+        allow_filetypes = {  -- enable format on save for specified filetypes only
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
