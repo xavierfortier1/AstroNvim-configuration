2832d0d97fa362b4c701bf8492c98fc051e7a2e1
Xavier Fortier <xabellechasse20@gmail.com>
2023-06-25 00:27:03 -0400

    Added folke/flash.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -108,4 +108,31 @@ return {
       show_current_context = true,
     },
   },
+
+  -- flash.nvim
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "s",
+        moe = { "n", "x", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function() require("flash").remote() end,
+        desc = "Remote Flash",
+      },
+    },
+  },
 }
