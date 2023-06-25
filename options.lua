53791c69b39fb136e13d11cb171dada9c3d5b783
Xavier Fortier <xabellechasse20@gmail.com>
2023-06-25 12:20:21 -0400

    Added clipboard

--- options.lua
+++ options.lua
@@ -24,6 +24,7 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     noswapfile = true,
+    clipboard = unnamedplus,
   },
 }
 -- If you need more control, you can use the function()...end notation
