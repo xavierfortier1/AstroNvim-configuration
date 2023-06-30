c51099bd6ee8098a86d1cca5fdfcb0f8ad95ae24
Xavier Fortier <xabellechasse20@gmail.com>
2023-06-30 15:30:40 -0400

    Add rust-analyzer config

--- init.lua
+++ init.lua
@@ -45,6 +45,18 @@ return {
     servers = {
       -- "pyright"
     },
+    config = {
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+              extraArgs = { "--profile", "rust-analyzer" },
+            },
+          },
+        },
+      },
+    },
   },
   -- Configure require("lazy").setup() options
   lazy = {
